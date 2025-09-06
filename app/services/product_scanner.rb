25f37b4227ca5fd476d9ad2c9efa7f54e68822d3
Julius Blanco <jcfblanco@gmail.com>
2025-09-06 13:26:03 +0800

    Add ProductScanner service

---
+++ app/services/product_scanner.rb
@@ -0,0 +1,20 @@
+class ProductScanner
+  attr_reader :basket
+
+  def initialize(basket)
+    @basket = basket
+  end
+
+  def scan(product_code)
+    product = Product.find_by(product_code:)
+    return 'INVALID' unless product
+
+    existing_item = basket.basket_items.find_by(product:)
+
+    if existing_item
+      existing_item.increment!(:quantity)
+    else
+      basket.basket_items.create(product:, quantity: 1)
+    end
+  end
+end
