890bdbdb686c9ce750cd65ee9a216d04faca083c
Julius Blanco <jcfblanco@gmail.com>
2025-09-07 13:55:27 +0800

    Initial implementation of price calculation

--- app/services/product_scanner.rb
+++ app/services/product_scanner.rb
@@ -9,14 +9,29 @@ class ProductScanner
     product = Product.find_by(product_code:)
     return false unless product
 
+    basket_item = add_product_to_basket(product)
+    calculate_basket_item_price(basket_item)
+
+    true
+  end
+
+  private
+
+  def add_product_to_basket(product)
     existing_item = basket.basket_items.find_by(product:)
 
     if existing_item
-      existing_item.increment!(:quantity)
+      existing_item.tap do |item|
+        item.quantity += 1
+        item.price_cents = product.price_cents * item.quantity
+        item.save!
+      end
     else
-      basket.basket_items.create!(product:, quantity: 1)
+      basket.basket_items.create!(product:, quantity: 1, price_cents: product.price_cents)
     end
+  end
 
-    true
+  def calculate_basket_item_price(basket_item)
+    basket_item.update!(price_cents: basket_item.product.price_cents * basket_item.quantity)
   end
 end
--- spec/services/product_scanner_spec.rb
+++ spec/services/product_scanner_spec.rb
@@ -53,6 +53,52 @@ RSpec.describe ProductScanner do
         expect { scanner.scan(product.product_code) }.to change { basket.basket_items.where(product:).count }.by(0)
         expect { scanner.scan(product.product_code) }.to change { basket.basket_items.find_by(product:).quantity }.by(1)
       end
+
+      it 'sets the correct price when adding a new product' do
+        scanner.scan(product.product_code)
+        basket_item = basket.basket_items.find_by(product:)
+        expect(basket_item.price_cents).to eq(product.price_cents * 1)
+      end
+
+      it 'updates the price when increasing quantity of existing product' do
+        scanner.scan(product.product_code)
+        initial_price = basket.basket_items.find_by(product:).price_cents
+
+        scanner.scan(product.product_code)
+        updated_basket_item = basket.basket_items.find_by(product:)
+
+        expect(updated_basket_item.price_cents).to eq(product.price_cents * updated_basket_item.quantity)
+        expect(updated_basket_item.price_cents).to be > initial_price
+      end
+
+      context 'with different priced products' do
+        let(:expensive_product) { create(:product, product_code: 'EXP', price_cents: 2500) }
+        let(:cheap_product) { create(:product, product_code: 'CHP', price_cents: 500) }
+
+        it 'calculates correct prices for different products' do
+          scanner.scan(expensive_product.product_code)
+          scanner.scan(cheap_product.product_code)
+
+          expensive_item = basket.basket_items.find_by(product: expensive_product)
+          cheap_item = basket.basket_items.find_by(product: cheap_product)
+
+          expect(expensive_item.price_cents).to eq(2500)
+          expect(cheap_item.price_cents).to eq(500)
+        end
+
+        it 'calculates correct total price when scanning multiple quantities' do
+          3.times { scanner.scan(expensive_product.product_code) }
+          2.times { scanner.scan(cheap_product.product_code) }
+
+          expensive_item = basket.basket_items.find_by(product: expensive_product)
+          cheap_item = basket.basket_items.find_by(product: cheap_product)
+
+          expect(expensive_item.quantity).to eq(3)
+          expect(expensive_item.price_cents).to eq(2500 * 3)
+          expect(cheap_item.quantity).to eq(2)
+          expect(cheap_item.price_cents).to eq(500 * 2)
+        end
+      end
     end
 
     context 'when scanning an invalid product code' do
